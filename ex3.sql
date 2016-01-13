1192477f09e019190ba91bec07e39a86e0bfa6ec
Harry Moreno <morenoh149@gmail.com>
2016-01-13 18:05:09 -0500

    first

---
+++ ex3.sql
@@ -0,0 +1,7 @@
+insert into person (id, first_name, last_name, age)
+	values (0, 'zed', 'shaw', 37);
+
+insert into pet (id, name, breed, age, dead)
+	values (0, 'fluffy', 'unicorn', 1000, 0);
+
+insert into pet values (1, 'gigantor', 'robot', 1, 1);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# learn sequel the hard way
+
+* solutions
